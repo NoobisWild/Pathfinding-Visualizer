d27aad6f7e7f75f9fcf0e8ba98b1bb8ba5bbe730
Himanshu Rana <ranahiman2000@gmail.com>
2024-11-06 15:34:32 +0530

    Tile component

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { usePathfinding } from "../hooks/usePathfinding";
 import { MAX_COLS, MAX_ROWS } from "../utils/constants";
+import { Tile } from "./Tile";
 
 
 export function Grid() {
@@ -23,10 +24,22 @@ export function Grid() {
         >
             {grid.map((row, rowIndex) => (
             <div key={rowIndex} className="flex">
-                {row.map((tile, tileIndex) => (
-
-                <div className="bg-white h-2 w-2 border" />
-                ))}
+                {row.map((tile, tileIndex) => {
+                    const {isEnd, isStart, isPath, isTraversed, isWall} = tile;
+                    return (
+                        <Tile 
+                        key={tileIndex}
+                        row={tile.row}
+                        col={tile.col}
+                        isEnd={isEnd}
+                        isStart={isStart}
+                        isPath={isPath}
+                        isTraversed={isTraversed}
+                        isWall={isWall}
+                        />
+                    )
+                
+})}
             </div>
             ))}
         </div>
